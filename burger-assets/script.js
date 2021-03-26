9a2773dbe408690f9c00c5b803555603c2d983d9
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-26 16:45:06 +0100

    ingredienti

--- burger-assets/script.js
+++ burger-assets/script.js
@@ -2,12 +2,33 @@
 
 var bot = document.getElementById('button');
 console.log(bot);
+var nomeBurger = document.getElementById('name');
+console.log(nomeBurger); 
+var ingredienti = document.getElementsByClassName('ingredient-checkbox')
+console.log(ingredienti);
 
 
 
-//calcolare il prezzo
-
+//click sul bottone
 
 bot.addEventListener('click', function(){
-    console.log('click')
+    console.log('click!!!!!!!!');
+    var name = nomeBurger.value.trim();
+    console.log(name);
+    
+    //Nome burger obbligatori
+
+    if(name.lenght === 0) {
+        alert('Il tuo panino non ha un nome :(');//non esce l'alert
+
+    } else {
+        //console.log('calcolo prezzo...');
+
+        //aggiunta costo ingredienti
+
+        var price = 50; 
+
+
+    }
+     
 });

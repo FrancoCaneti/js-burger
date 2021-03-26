2504e76b352059caa2014c32d79477dac7dcfdc7
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-26 17:28:50 +0100

    prezzo hamburger

--- burger-assets/script.js
+++ burger-assets/script.js
@@ -4,31 +4,47 @@ var bot = document.getElementById('button');
 console.log(bot);
 var nomeBurger = document.getElementById('name');
 console.log(nomeBurger); 
-var ingredienti = document.getElementsByClassName('ingredient-checkbox')
+var ingredienti = document.getElementsByClassName('ingredient-checkbox');
 console.log(ingredienti);
-
+var prezzoDisplay = document.getElementById('price');
 
 
 //click sul bottone
 
 bot.addEventListener('click', function(){
     console.log('click!!!!!!!!');
-    var name = nomeBurger.value.trim();
-    console.log(name);
+    var nome = nomeBurger.value.trim();
+    console.log(nome);
     
     //Nome burger obbligatori
 
-    if(name.lenght === 0) {
-        alert('Il tuo panino non ha un nome :(');//non esce l'alert
+    if(nome.length === 0) {
+        alert('Il tuo panino non ha un nome :(');//non esce l'alert //risolto
 
     } else {
         //console.log('calcolo prezzo...');
 
-        //aggiunta costo ingredienti
+        //aggiunta costo ingredienti su prezzo base
 
         var price = 50; 
+  
+        for ( var i = 0; i < ingredienti.length; i++){ 
+            var ingredientilist = ingredienti[i];
+            //console.log(ingredientilist.checked);
 
 
+            if(ingredientilist.checked === true){
+                console.log(ingredientilist.value);
+
+
+        //aggiunta valore prezzo
+             price += parseInt(ingredientilist.value);
+            }
+        }
+
+        console.log(price);
+        //prezzo a schermo
+        prezzoDisplay.innerHtml = price.toFixed(2); 
     }
      
 });

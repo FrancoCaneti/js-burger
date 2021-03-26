5b47b321fb5d76ae33912e6fa556d16d7aa5dfd4
FrancoCaneti <CAnetifranco95@hmail.com>
2021-03-26 16:03:14 +0100

    click botton

---
+++ burger-assets/script.js
@@ -0,0 +1,13 @@
+// Bottone calcolo prezzo
+
+var bot = document.getElementById('button');
+console.log(bot);
+
+
+
+//calcolare il prezzo
+
+
+bot.addEventListener('click', function(){
+    console.log('click')
+});
